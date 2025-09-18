680a7e191634e8ef00f788f5ca362e500abe44dc
arbadsha00 <arbadsha00@gmail.com>
2025-09-18 22:52:20 +0600

    BrignJoy Section Done

---
+++ src/app/(public)/_components/BringJoy/BringJoy.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Image from "next/image";
+
+export default function BringJoy() {
+  return (
+    <div className="flex flex-col lg:flex-row items-center justify-center gap-3 md:gap-6 lg:gap-10 text-center">
+      <div className="space-y-3 md:space-y-6">
+        <h1 className="text-2xl md:text-5xl lg:text-6xl font-semibold lg:text-right">
+          Spread Smiles
+          <br />
+          Create Change
+        </h1>
+        <p className="max-w-3xl text-slate-600 text-sm md:text-base lg:text-right">
+          Your generosity can transform ordinary days into unforgettable smiles
+          for people in need. Together, we can turn meals into moments of joy
+          and hope for those who need it most.
+        </p>
+      </div>
+
+      <Image src="/bringjoy.webp" className="rounded-2xl" alt="Hero image" width={640} height={100} />
+    </div>
+  );
+}
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,11 +1,13 @@
 "use client";
+import BringJoy from "./_components/BringJoy/BringJoy";
 import Hero from "./_components/Hero/Hero";
 import SDGs from "./_components/SDGs/SDGs";
 
 export default function HomePage() {
   return (
-    <div className="space-y-20 md:space-y-40">
+    <div className="space-y-20 md:space-y-64">
       <Hero />
+      <BringJoy />
      <SDGs />
      
     </div>
